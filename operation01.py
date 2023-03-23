fbd68e8378fa11d04e27250622bafa4bd0186bf7
ShianZz <128707497+ShianZz@users.noreply.github.com>
2023-03-23 20:52:17 +0800

    Add files via upload
    老師在幫我看一下這樣可以嗎

---
+++ operation01.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 06:39:16 2023
+
+@author: USER
+"""
+
+print("*"*30)
+print("作業1")
+for a in range(2,10):
+    for b in range(1,10,+1):
+   
+        print(a,"*",b,"=",a*b,sep="",end="\n")
+       
+    print()
+
+
+print("*"*30)
+print()
+
+
+print("作業2")
+for i in range(1,8,+2):
+    for y in range(1,i+1):
+        y=1
+        print(y,end="")
+    print()
+
+
+print("*"*30)
+print()
+
+
+print("作業3")
+for i in range(9,1,-1):
+    for y in range(1,i+1,):
+        print(i,end="")
+    print()   
+for i in range(1,10):
+    for y in range(1,i+1):
+        print(i,end="")
+    print()
+
+    
+print("*"*30)
+print()
+
+
+'''
+print("作業4")
+a = int(input("請輸入第一邊的長度："))
+b = int(input("請輸入第二邊的長度："))
+c = int(input("請輸入第三邊的長度："))
+
+if a + b > c and b + c > a and a + c > b:
+    print("可以構成三角形")
+else:
+    print("無法構成三角形")
+    
+print("*"*30)
+print()
+
+
+print("作業4-1")
+a = int(input("請輸入第一邊的長度："))
+b = int(input("請輸入第二邊的長度："))
+c = int(input("請輸入第三邊的長度："))
+
+if a == c or b == a or a == b:
+    print("可以構成等腰三角形")
+else:
+    print("無法構成等腰三角形")
+    
+print("*"*30)
+print()
+   
+
+print("作業4-2")
+a = int(input("請輸入第一邊的長度："))
+b = int(input("請輸入第二邊的長度："))
+c = int(input("請輸入第三條的長度："))
+
+if a == b == c :
+    print("可以構成正三角形")
+else:
+    print("無法構成正三角形")
+'''   
+
+
